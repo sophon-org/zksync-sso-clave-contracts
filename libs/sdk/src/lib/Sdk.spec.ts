a42b62aa9b366e8b21936ea5c3d2ba1b667179f7
Sabrina Ferguson <sf@matterlabs.dev>
2024-08-22 17:01:43 -0400

    create sdk lib

---
+++ libs/sdk/src/lib/Sdk.spec.ts
@@ -0,0 +1,7 @@
+import { sdk } from './Sdk';
+
+describe('sdk', () => {
+  it('should work', () => {
+    expect(sdk()).toEqual('sdk');
+  });
+});
---
+++ libs/sdk/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/libs/sdk',
+
+  plugins: [nxViteTsPaths()],
+
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: { reportsDirectory: '../../coverage/libs/sdk', provider: 'v8' },
+  },
+});
