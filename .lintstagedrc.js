7053ecbc2cd81ba05f13271d97028adcc93156a0
Sabrina Ferguson <sf@matterlabs.dev>
2024-10-04 15:21:19 -0400

    chore: nx command edits

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ export default {
   "*.{js,ts,vue}": ["eslint --no-ignore"],
   "*.md": ["markdownlint-cli2", "cspell lint --quiet --no-must-find-files --files"],
   "*.{json,yml}": ["prettier --list-different"],
-  "*.{sol}": ["prettier --list-different"],
+  "*.sol": ["prettier --list-different"],
 };
