5fecec69a27aa4f307859c20acef2624d46acebe
Jack Hamer <jackhamer0990@gmail.com>
2024-10-07 17:48:23 +0300

    fix: update sdk dist logic

---
+++ packages/sdk/scripts/postbuild.ts
@@ -0,0 +1,28 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+
+// Define paths for the source and destination package.json files
+const srcPath = join(import.meta.dirname, "../", "src", "package.json");
+const distPath = join(import.meta.dirname, "../", "dist", "package.json");
+
+// Read the package.json file from src
+let packageJson = JSON.parse(readFileSync(srcPath, "utf-8"));
+
+// Update name
+packageJson = {
+  name: "zksync-account",
+  ...packageJson,
+};
+
+// Remove unwanted fields
+delete packageJson.publishConfig;
+delete packageJson.private;
+
+// NOTE: We explicitly don't want to publish the type field.
+// We create a separate package.json for `dist/cjs` and `dist/esm` that has the type field.
+delete packageJson.type;
+
+// Write the modified package.json to dist
+writeFileSync(distPath, JSON.stringify(packageJson, null, 2));
+
+console.log("package.json copied to dist/");
