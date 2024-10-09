56e5f480b6cb06829ca7cb4265021584612a7086
MexicanAce <nicolasvillanueva@msn.com>
2024-10-09 11:41:30 +0100

    chore: simplify e2e test to start demo-app and gateway

--- packages/demo-app/playwright.config.ts
+++ packages/demo-app/playwright.config.ts
@@ -71,9 +71,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+      command: "pnpm nx dev demo-app",
+      url: "http://localhost:3004",
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: "pnpm nx serve gateway",
+      url: "http://localhost:3002",
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
