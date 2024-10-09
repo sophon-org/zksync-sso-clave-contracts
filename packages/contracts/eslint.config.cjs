90dd5cdda963c99f3be83d7f98253de473a631b8
Sabrina Ferguson <sf@matterlabs.dev>
2024-10-09 12:00:31 -0400

    chore: skip tests

--- packages/contracts/eslint.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = (async () => {
-  const baseConfig = await import("../../eslint.config.js").then((module) => module.default || module);
-  return [
-    { ignores: ["node_modules", "dist", "temp", "tmp", "tests", "test", "artifacts-zk", "cache-zk", "deployments-zk", "typechain-types"] },
-    ...baseConfig,
-  ];
-})();
