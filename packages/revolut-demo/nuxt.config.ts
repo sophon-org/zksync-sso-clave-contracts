70b199519a5e3983af2b4af7ee82f27d33505e27
MexicanAce <nicolasvillanueva@msn.com>
2024-10-18 10:22:34 +0100

    fix: default addresses

--- packages/revolut-demo/nuxt.config.ts
+++ packages/revolut-demo/nuxt.config.ts
@@ -21,12 +21,12 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      aaveAddress: "0xE90E12261CCb0F3F7976Ae611A29e84a6A85f424", // Rich Account 9
+      aaveAddress: "0xBC989fDe9e54cAd2aB4392Af6dF60f04873A033A", // Rich Account 0
       revolutDemoDeployerKey: "0x3d3cbc973389cb26f657686445bcc75662b415b656078503592ac8c1abb8810e", // Rich Account 0
       network: zksyncInMemoryNode,
       session: "0x476F23ef274F244282252341792c8a610feF78ee",
       passkey: "0x455e8d86DC6728396f8d3B740Fc893F4E20b25Dc",
-      accountFactory: "0x35938C70af13d0c3bBb4e852A9Ab10B20797AeD5",
+      accountFactory: "0x23b13d016E973C9915c6252271fF06cCA2098885",
       accountImplementation: "0x6cd5A2354Be0E656e7A1E94F1C0570E08EC4789B",
     }
   },
