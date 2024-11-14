a5f5123c7962f72cbfedaaf5b137b1afda17e37a
Jack Hamer <jackhamer0990@gmail.com>
2024-11-14 12:05:56 +0200

    Reapply "fix: try fix estimation"
    This reverts commit 4092f841cca150ea6ce25493b345fc8296c3af13.

--- packages/sdk/src/client/decorators/publicActionsRewrite.ts
+++ packages/sdk/src/client/decorators/publicActionsRewrite.ts
@@ -1,5 +1,5 @@
 import type { Account, Chain, Hex, PublicActions, Transport } from "viem";
-import { estimateContractGas, estimateGas } from "viem/actions";
+import { estimateContractGas, estimateGas, getGasPrice, prepareTransactionRequest } from "viem/actions";
 
 import { type ClientWithZksyncAccountSessionData, signSessionTransaction } from "../clients/session.js";
 
@@ -11,9 +11,32 @@ export function publicActionsRewrite<
   account extends Account,
 >(
   client: ClientWithZksyncAccountSessionData<transport, chain, account>,
-): Pick<PublicActions<transport, chain, account>, "estimateContractGas" | "estimateGas"> {
+): Pick<PublicActions<transport, chain, account>, "estimateContractGas" | "estimateGas" | "prepareTransactionRequest"> {
   return {
+    prepareTransactionRequest: async (args) => {
+      console.log("123prepareTransactionRequest");
+      if (!("customSignature" in args)) {
+        (args as any).customSignature = signSessionTransaction({
+          sessionKeySignedHash: emptySignature,
+          sessionContract: client.contracts.session,
+          sessionConfig: client.sessionConfig,
+        });
+      }
+      console.log("Initial args", args);
+      const request = await prepareTransactionRequest(client as any, {
+        ...args,
+        type: "eip712",
+        typeHex: "0x71",
+        chainId: client.chain.id,
+        parameters: ["gas", "nonce"],
+        gasPrice: await getGasPrice(client),
+        gas: await estimateGas(client, args as any),
+      } as any) as any;
+      console.log("Request", request);
+      return request;
+    },
     estimateContractGas: (args) => {
+      console.log("123estimateContractGas");
       if (!("customSignature" in args)) {
         (args as any).customSignature = signSessionTransaction({
           sessionKeySignedHash: emptySignature,
@@ -24,6 +47,7 @@ export function publicActionsRewrite<
       return estimateContractGas(client, args as any);
     },
     estimateGas: async (args) => {
+      console.log("123estimateGas");
       if (!("customSignature" in args)) {
         (args as any).customSignature = signSessionTransaction({
           sessionKeySignedHash: emptySignature,
@@ -31,7 +55,9 @@ export function publicActionsRewrite<
           sessionConfig: client.sessionConfig,
         });
       }
-      return await estimateGas(client, args);
+      const estimated = await estimateGas(client, args);
+      console.log("Estimated", estimated);
+      return estimated;
     },
   };
 }
