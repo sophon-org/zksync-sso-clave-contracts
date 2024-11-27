7c5b70c4457b268d96c9dcc16730074fab6eb114
Colin <cbe@matterlabs.dev>
2024-11-27 09:49:39 -0800

    fix: remove examples from top level
    this was confusing nx when it was trying to build just contracts

--- examples/bank-demo/components/zk/panel/Card.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="border border-neutral-200 rounded-zk bg-white p-4 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-100"
-  >
-    <slot/>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
