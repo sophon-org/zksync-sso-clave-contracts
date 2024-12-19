795c857ff01df7afc75ae68518e9a523b7daacf9
cpb8010 <cpb8010@users.noreply.github.com>
2024-12-19 02:11:04 -0800

    feat: remove uint2str (#225)
    Unused and doesn't work great according to the audit
    
    Co-authored-by: Nicolas Villanueva <1890113+MexicanAce@users.noreply.github.com>

--- src/libraries/JsmnSolLib.sol
+++ src/libraries/JsmnSolLib.sol
@@ -334,23 +334,6 @@ library JsmnSolLib {
     return mint;
   }
 
-  function uint2str(uint i) internal pure returns (string memory) {
-    if (i == 0) return "0";
-    uint j = i;
-    uint len;
-    while (j != 0) {
-      len++;
-      j /= 10;
-    }
-    bytes memory bstr = new bytes(len);
-    uint k = len - 1;
-    while (i != 0) {
-      bstr[k--] = bytes1(uint8(48 + (i % 10)));
-      i /= 10;
-    }
-    return string(bstr);
-  }
-
   function parseBool(string memory _a) internal pure returns (bool) {
     if (strCompare(_a, "true") == 0) {
       return true;
