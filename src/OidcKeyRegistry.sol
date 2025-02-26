0be0da40117aa51f700df08ccb0991db67df7ed5
Matías Ignacio González <maigonzalez@fi.uba.ar>
2025-02-26 11:20:07 -0300

    feat: Add merkle tree computation to KeyRegistry (#314)
    * feat: param OIDCKeys is now an array
    
    OIDCKeys is now an array
    
    * feat: change max keys to 8
    
    change max keys to 8
    
    * feat: add _computeMerkleRoot
    
    add _computeMerkleRoot
    
    * feat: addKeys doesnt calculate multiple merkle roots
    
    addKeys doesnt calculate multiple merkle roots
    
    * refactor: add _hashKey
    
    add _hashKey
    
    * refactor: add _updateMerkleRoot
    
    add _updateMerkleRoot
    
    * fix: add merkle to dict
    
    add merkle to dict
    
    * feat: move merkle root to slot 1
    
    move merkle root to slot 1
    
    * feat: add verifyKey
    
    add verifyKey
    
    * Update test/OidcKeyRegistryTest.ts
    
    * refactor: remove casts
    
    remove casts
    
    * docs: add merkle root doc
    
    add merkle root doc

--- src/OidcKeyRegistry.sol
+++ src/OidcKeyRegistry.sol
@@ -3,19 +3,25 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract OidcKeyRegistry is Initializable, OwnableUpgradeable {
-  uint8 public constant MAX_KEYS = 5;
+  uint8 public constant MAX_KEYS = 8;
+  // Merkle root is used to validate externally provided keys during transaction validation,
+  // as only this storage slot is accessible in the validation step due to EIP-7562 validation rules
+  // (see: https://eips.ethereum.org/EIPS/eip-7562#validation-rules).
+  // This enables key verification without requiring access to the full key registry.
+  bytes32 public merkleRoot; // Merkle root should be on slot 1
 
   struct Key {
+    bytes32 issHash; // Issuer
     bytes32 kid; // Key ID
     bytes n; // RSA modulus
     bytes e; // RSA exponent
   }
 
-  // Mapping uses keccak256(iss) as the key
-  mapping(bytes32 => Key[MAX_KEYS]) public OIDCKeys; // Stores up to MAX_KEYS per issuer
-  mapping(bytes32 => uint8) public keyIndexes; // Tracks the latest key index for each issuer
+  Key[MAX_KEYS] public OIDCKeys;
+  uint8 public keyIndex;
 
   constructor() {
     initialize();
@@ -23,33 +29,79 @@ contract OidcKeyRegistry is Initializable, OwnableUpgradeable {
 
   function initialize() public initializer {
     __Ownable_init();
+    keyIndex = MAX_KEYS - 1;
   }
 
   function hashIssuer(string memory iss) public pure returns (bytes32) {
     return keccak256(abi.encodePacked(iss));
   }
 
-  function setKey(bytes32 issHash, Key memory key) public onlyOwner {
-    uint8 index = keyIndexes[issHash];
-    uint8 nextIndex = (index + 1) % MAX_KEYS; // Circular buffer
-    OIDCKeys[issHash][nextIndex] = key;
-    keyIndexes[issHash] = nextIndex;
+  function addKey(Key memory newKey) public onlyOwner {
+    Key[] memory newKeys = new Key[](1);
+    newKeys[0] = newKey;
+    addKeys(newKeys);
   }
 
-  function setKeys(bytes32 issHash, Key[] memory keys) public onlyOwner {
-    for (uint8 i = 0; i < keys.length; i++) {
-      setKey(issHash, keys[i]);
+  function addKeys(Key[] memory newKeys) public onlyOwner {
+    uint8 nextIndex = keyIndex;
+    for (uint8 i = 0; i < newKeys.length; i++) {
+      nextIndex = (keyIndex + 1 + i) % MAX_KEYS; // Circular buffer
+      OIDCKeys[nextIndex] = newKeys[i];
     }
+
+    _updateMerkleRoot();
+    keyIndex = nextIndex;
   }
 
   function getKey(bytes32 issHash, bytes32 kid) public view returns (Key memory) {
+    require(issHash != 0, "Invalid issHash");
     require(kid != 0, "Invalid kid");
-    Key[MAX_KEYS] storage keys = OIDCKeys[issHash];
     for (uint8 i = 0; i < MAX_KEYS; i++) {
-      if (keys[i].kid == kid) {
-        return keys[i];
+      if (OIDCKeys[i].issHash == issHash && OIDCKeys[i].kid == kid) {
+        return OIDCKeys[i];
       }
     }
     revert("Key not found");
+  }
+
+  function verifyKey(Key memory key, bytes32[] memory proof) public view returns (bool) {
+    bytes32 leaf = _hashKey(key);
+    return MerkleProof.verify(proof, merkleRoot, leaf);
+  }
+
+  function _updateMerkleRoot() private {
+    bytes32[MAX_KEYS] memory leaves;
+    for (uint8 i = 0; i < MAX_KEYS; i++) {
+      leaves[i] = _hashKey(OIDCKeys[i]);
+    }
+    merkleRoot = _computeMerkleRoot(leaves);
+  }
+
+  function _hashKey(Key memory key) private pure returns (bytes32) {
+    return keccak256(bytes.concat(keccak256(abi.encode(key.issHash, key.kid, key.n, key.e))));
+  }
+
+  function _computeMerkleRoot(bytes32[MAX_KEYS] memory leaves) private pure returns (bytes32) {
+    uint256 n = leaves.length;
+    while (n > 1) {
+      for (uint256 i = 0; i < n / 2; i++) {
+        leaves[i] = _hashPair(leaves[2 * i], leaves[2 * i + 1]);
+      }
+      n = n / 2;
+    }
+    return leaves[0];
+  }
+
+  // Taken from OpenZeppelin's MerkleProof.sol
+  function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
+    return a < b ? _efficientHash(a, b) : _efficientHash(b, a);
+  }
+
+  function _efficientHash(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
+    assembly {
+      mstore(0x00, a)
+      mstore(0x20, b)
+      value := keccak256(0x00, 0x40)
+    }
   }
 }
--- test/OidcKeyRegistryTest.ts
+++ test/OidcKeyRegistryTest.ts
@@ -1,3 +1,5 @@
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import { ContractFixtures, getProvider } from "./utils";
 import { expect } from "chai";
 import { ethers } from "ethers";
 import { Wallet } from "zksync-ethers";
@@ -23,26 +25,64 @@ describe("OidcKeyRegistry", function () {
   });
 
   it("should set one key", async () => {
+    let keys = await Promise.all(Array.from({ length: 8 }, (_, i) => oidcKeyRegistry.OIDCKeys(i)));
+    const currentIndex = await oidcKeyRegistry.keyIndex();
+    const nextIndex = ((currentIndex + 1n) % 8n) as unknown as number;
+  
     const issuer = "https://example.com";
     const issHash = await oidcKeyRegistry.hashIssuer(issuer);
+  
     const key = {
+      issHash,
       kid: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
       n: "0xabcdef",
       e: "0x010001",
     };
-
-    await oidcKeyRegistry.setKey(issHash, key);
-
+  
+    keys[nextIndex] = [key.issHash, key.kid, key.n, key.e];
+  
+    const tree = StandardMerkleTree.of(keys, ["bytes32", "bytes32", "bytes", "bytes"]);
+    const root = tree.root;
+  
+    await oidcKeyRegistry.addKey(key);
+  
     const storedKey = await oidcKeyRegistry.getKey(issHash, key.kid);
     expect(storedKey.kid).to.equal(key.kid);
     expect(storedKey.n).to.equal(key.n);
     expect(storedKey.e).to.equal(key.e);
+    expect(await oidcKeyRegistry.merkleRoot()).to.equal(root);
+  });
+
+  it("should set multiple keys", async () => {
+    const issuer = "https://example.com";
+    const issHash = await oidcKeyRegistry.hashIssuer(issuer);
+    const newKeys = Array.from({ length: 8 }, (_, i) => ({
+      issHash,
+      kid: ethers.keccak256(ethers.toUtf8Bytes(`key${i + 1}`)),
+      n: "0xabcdef",
+      e: "0x010001",
+    }));
+
+    await oidcKeyRegistry.addKeys(newKeys);
+
+    for (let i = 0; i < 8; i++) {
+      const storedKey = await oidcKeyRegistry.getKey(issHash, newKeys[i].kid);
+      expect(storedKey.kid).to.equal(newKeys[i].kid);
+    }
+    
+    let anotherKeyRegistry = await fixtures.deployOidcKeyRegistryContract();
+    for (let i = 0; i < 8; i++) {
+      await anotherKeyRegistry.addKey(newKeys[i]);
+    }
+
+    expect(await oidcKeyRegistry.merkleRoot()).to.equal(await anotherKeyRegistry.merkleRoot());
   });
 
   it("should revert when a non-owner tries to set a key", async () => {
     const issuer = "https://example.com";
     const issHash = await oidcKeyRegistry.hashIssuer(issuer);
     const key = {
+      issHash,
       kid: "0x3333333333333333333333333333333333333333333333333333333333333333",
       n: "0xcccc",
       e: "0x010001",
@@ -51,48 +91,107 @@ describe("OidcKeyRegistry", function () {
     const nonOwner = Wallet.createRandom(getProvider());
     const nonOwnerRegistry = OidcKeyRegistry__factory.connect(await oidcKeyRegistry.getAddress(), nonOwner);
 
-    await expect(nonOwnerRegistry.setKey(issHash, key)).to.be.revertedWith("Ownable: caller is not the owner");
+    await expect(nonOwnerRegistry.addKey(key)).to.be.revertedWith("Ownable: caller is not the owner");
   });
 
   it("should correctly implement circular key storage", async () => {
     const issuer = "https://example.com";
     const issHash = await oidcKeyRegistry.hashIssuer(issuer);
 
-    const keys = Array.from({ length: 5 }, (_, i) => ({
+    const keys = Array.from({ length: 8 }, (_, i) => ({
+      issHash,
       kid: ethers.keccak256(ethers.toUtf8Bytes(`key${i + 1}`)),
       n: "0xabcdef",
       e: "0x010001",
     }));
 
-    for (const key of keys) {
-      await oidcKeyRegistry.setKey(issHash, key);
-    }
+    await oidcKeyRegistry.addKeys(keys);
 
     // Check that the keys are stored correctly
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 8; i++) {
       const storedKey = await oidcKeyRegistry.getKey(issHash, keys[i].kid);
       expect(storedKey.kid).to.equal(keys[i].kid);
     }
 
-    const moreKeys = Array.from({ length: 5 }, (_, i) => ({
-      kid: ethers.keccak256(ethers.toUtf8Bytes(`key${i + 6}`)),
+    const moreKeys = Array.from({ length: 8 }, (_, i) => ({
+      issHash,
+      kid: ethers.keccak256(ethers.toUtf8Bytes(`key${i + 9}`)),
       n: "0xabcdef",
       e: "0x010001",
     }));
 
-    for (const key of moreKeys) {
-      await oidcKeyRegistry.setKey(issHash, key);
-    }
+    await oidcKeyRegistry.addKeys(moreKeys);
 
     // Check that the new keys are stored correctly
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 8; i++) {
       const storedKey = await oidcKeyRegistry.getKey(issHash, moreKeys[i].kid);
       expect(storedKey.kid).to.equal(moreKeys[i].kid);
     }
 
     // Check that the old keys are not stored anymore
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 8; i++) {
       await expect(oidcKeyRegistry.getKey(issHash, keys[i].kid)).to.be.revertedWith("Key not found");
     }
+  });
+
+  it("should verify key with merkle proof", async () => {
+    let keys = await Promise.all(Array.from({ length: 8 }, (_, i) => oidcKeyRegistry.OIDCKeys(i)));
+    const currentIndex = await oidcKeyRegistry.keyIndex();
+    const nextIndex = ((currentIndex + 1n) % 8n) as unknown as number;
+  
+    const issuer = "https://example.com";
+    const issHash = await oidcKeyRegistry.hashIssuer(issuer);
+  
+    const key = {
+      issHash,
+      kid: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+      n: "0xabcdef",
+      e: "0x010001",
+    };
+  
+    keys[nextIndex] = [key.issHash, key.kid, key.n, key.e];
+  
+    const tree = StandardMerkleTree.of(keys, ["bytes32", "bytes32", "bytes", "bytes"]);
+  
+    await oidcKeyRegistry.addKey(key);
+    
+    const proof = tree.getProof([key.issHash, key.kid, key.n, key.e]);
+    
+    const isValid = await oidcKeyRegistry.verifyKey(key, proof);
+    expect(isValid).to.be.true;
+  });
+
+  it("verifyKey should return false when the key is not in the registry", async () => {
+    let keys = await Promise.all(Array.from({ length: 8 }, (_, i) => oidcKeyRegistry.OIDCKeys(i)));
+    const currentIndex = await oidcKeyRegistry.keyIndex();
+    const nextIndex = ((currentIndex + 1n) % 8n) as unknown as number;
+  
+    const issuer = "https://example.com";
+    const issHash = await oidcKeyRegistry.hashIssuer(issuer);
+  
+    const key = {
+      issHash,
+      kid: "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+      n: "0xabcdef",
+      e: "0x010001",
+    };
+
+    const nonExistentKey = {
+      issHash,
+      kid: "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+      n: "0xabcdef",
+      e: "0x010001",
+    };
+  
+    keys[nextIndex] = [key.issHash, key.kid, key.n, key.e];
+  
+    const tree = StandardMerkleTree.of(keys, ["bytes32", "bytes32", "bytes", "bytes"]);
+  
+    await oidcKeyRegistry.addKey(key);
+    
+    const proof = tree.getProof([key.issHash, key.kid, key.n, key.e]);
+    
+    const isValid = await oidcKeyRegistry.verifyKey(nonExistentKey, proof);
+    expect(isValid).to.be.false;
   });
 });
