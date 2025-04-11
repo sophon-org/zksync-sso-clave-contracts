897b0eb8f786f1f4c055143aa1801a63ba3c2025
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2025-04-11 03:55:08 +0300

    fix: simplify ERC1271 validation (#391)
    Co-authored-by: cpb8010 <cpb8010@users.noreply.github.com>

--- src/handlers/ERC1271Handler.sol
+++ src/handlers/ERC1271Handler.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 import { IERC1271Upgradeable } from "@openzeppelin/contracts-upgradeable/interfaces/IERC1271Upgradeable.sol";
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 import { SignatureDecoder } from "../libraries/SignatureDecoder.sol";
 import { IModuleValidator } from "../interfaces/IModuleValidator.sol";
@@ -14,13 +13,7 @@ import { ValidatorManager } from "../managers/ValidatorManager.sol";
 /// @author Matter Labs
 /// @notice Contract which provides ERC1271 signature validation
 /// @notice The implementation is inspired by Clave wallet.
-abstract contract ERC1271Handler is IERC1271Upgradeable, EIP712("Sso1271", "1.0.0"), OwnerManager, ValidatorManager {
-  struct SsoMessage {
-    bytes32 signedHash;
-  }
-
-  bytes32 private constant _SSO_MESSAGE_TYPEHASH = keccak256("SsoMessage(bytes32 signedHash)");
-
+abstract contract ERC1271Handler is IERC1271Upgradeable, OwnerManager, ValidatorManager {
   bytes4 private constant _ERC1271_MAGIC = 0x1626ba7e;
 
   /**
@@ -38,32 +31,9 @@ abstract contract ERC1271Handler is IERC1271Upgradeable, EIP712("Sso1271", "1.0.
 
     (bytes memory decodedSignature, address validator) = SignatureDecoder.decodeSignatureNoHookData(signature);
 
-    bytes32 eip712Hash = _hashTypedDataV4(_ssoMessageHash(SsoMessage(hash)));
-
     bool isValid = _isModuleValidator(validator) &&
-      IModuleValidator(validator).validateSignature(eip712Hash, decodedSignature);
+      IModuleValidator(validator).validateSignature(hash, decodedSignature);
 
     magicValue = isValid ? _ERC1271_MAGIC : bytes4(0);
-  }
-
-  /**
-   * @notice Returns the EIP-712 hash of the Sso message
-   * @param ssoMessage SsoMessage calldata - The message containing signedHash
-   * @return bytes32 - EIP712 hash of the message
-   */
-  function getEip712Hash(SsoMessage calldata ssoMessage) external view returns (bytes32) {
-    return _hashTypedDataV4(_ssoMessageHash(ssoMessage));
-  }
-
-  /**
-   * @notice Returns the typehash for the sso message struct
-   * @return bytes32 - Sso message typehash
-   */
-  function ssoMessageTypeHash() external pure returns (bytes32) {
-    return _SSO_MESSAGE_TYPEHASH;
-  }
-
-  function _ssoMessageHash(SsoMessage memory ssoMessage) private pure returns (bytes32) {
-    return keccak256(abi.encode(_SSO_MESSAGE_TYPEHASH, ssoMessage.signedHash));
   }
 }
